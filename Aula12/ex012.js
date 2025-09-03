77e4f874cfaa6193c503df4096158af328b8fc44
Guilherme Turra <g8turra@gmail.com>
2025-09-03 14:21:47 -0300

    Upload

---
+++ Aula12/ex012.js
@@ -0,0 +1,10 @@
+var hora = 
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12) {
+    console.log('Tenha um bom dia!!')
+} else if (hora <= 18) {
+    console.log('Tenha uma boa tarde!!')
+} else {
+    console.log('Boa Noite!!')
+}
